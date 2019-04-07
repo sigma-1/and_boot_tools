eb0f5012613356d445b0fe8228c4b963e3b7d73a
Vidir Alexander Jonsson <valex@valex.is>
2019-04-07 09:38:32 +0000

    include: Remove redundant copy of sys/cdefs.h
    Signed-off-by: Vidir Alexander Jonsson <valex@valex.is>

--- include/sys/cdefs.h
+++
@@ -1,9 +0,0 @@
-#if defined(__cplusplus)
-#define	__BEGIN_DECLS		extern "C" {
-#define	__END_DECLS		}
-#define	__static_cast(x,y)	static_cast<x>(y)
-#else
-#define	__BEGIN_DECLS
-#define	__END_DECLS
-#define	__static_cast(x,y)	(x)y
-#endif
